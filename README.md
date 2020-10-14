2d2eb1a5ae6975324f7dd52cba71a4ae8c3f119f
Timothy Salazar <timothy.salazar@colorado.edu>
2020-10-13 18:45:20 -0600

    adding doc strings. cosmetic refactoring

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Bluefruit UART Utilities
 
 This repo contains some utilities for using my Raspberry Pi to talk to a Bluefruit UART module. I'm using it for some quick debugging - I'm trying to send signals to the Bluefruit with a microcontroller, and it's useful to be able to do some debugging with another device.
+
+## BLE Keyboard Interface
+
+I enable the GATT over HID keyboard support with:
+
+AT+BLEHIDEN=1
+
+ATZ
+
+### Send keys
+
+AT+BLEKEYBOARD=something I send
--- send_uart.py
+++ send_uart.py
@@ -7,13 +7,15 @@ from datetime import datetime
 INPUT_TIMEOUT = 10
 MODE_PIN = 18
 
-def read_bytes(s):
-	received_bytes = ''
-	while s.in_waiting:
-		received_bytes += s.read()
-	return received_bytes
 
 def set_mode(mode):
+	"""	Input:
+			mode: bool - if True, we will set the mode
+				pin to command mode. If False, it will
+				be set to UART mode.
+		This sets the mode. While we're doing setup, it
+		also checks to see that the GPIO mode is set to BCM.
+	"""
 	if GPIO.getmode() != GPIO.BCM:
 		GPIO.setmode(GPIO.BCM)
 	if GPIO.gpio_function(MODE_PIN) == GPIO.OUT:
@@ -26,23 +28,58 @@ def set_mode(mode):
 		print("Command Mode")
 		GPIO.output(MODE_PIN, GPIO.HIGH)
 
+def read_bytes(s):
+	"""	Input:
+			s: Serial.serial
+		This takes bytes from Serial port and appends them to
+		a string for as long as there are bytes waiting.
+	"""
+	received_bytes = ''
+	while s.in_waiting:
+		received_bytes += s.read()
+	return received_bytes
 
-
-def main(mode,msg):
-	set_mode(mode)
-	time.sleep(.3)
+def get_serial():
+	""" Returns a Serial.serial object, which lets us read from
+		and write to the Bluefruit using UART
+	"""
 	s = serial.Serial("/dev/ttyS0",
 						baudrate = 9600,
 						timeout = INPUT_TIMEOUT)
-	s.write(msg+'\r\n')
-	print('> '+msg)
-	print('waiting for return bytes')
+	return s
+
+def wait_for_reply(s):
+	"""	Input:
+			s: serial.Serial object
+		Output:
+			s
+	This function waits INPUT_TIMEOUT seconds for a reply. Every
+	.3 seconds it checks to see if it's received a response, if
+	it has, it passes s to the read_bytes() function, prints
+	the result, and returns.
+	"""
 	start_time = datetime.now()
 	while (datetime.now() - start_time).seconds < INPUT_TIMEOUT:
 		time.sleep(.3)
 		if s.in_waiting:
 			print(read_bytes(s))
-			break
+			return s
+	return s
+
+def main(mode, msg):
+	""" Input:
+			mode: if True, the message is sent in command mode. 
+				  if False, the message is sent in UART mode
+			msg: string - the message to be sent to the Bluefruit 
+				 module
+
+	"""
+	set_mode(mode)
+	time.sleep(.3)
+	s = get_serial()
+	s.write(msg+'\r\n')
+	wait_for_reply(s)
+	print('waiting for return bytes')
 	GPIO.cleanup()
 	s.close()
 
